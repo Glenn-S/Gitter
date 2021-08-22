671feaf8317faba4c0023afaa0d2f346aff27e4f
Glenn Skelton <skeltong2@gmail.com>
2021-08-22 13:07:31 -0600

    Wiring up tailwind

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,65 +1,80 @@
 import { ReactElement, useCallback, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
 import { GitClient } from "../git/git-client";
 import { GitLogFormat } from '../git/git-constants';
 import { Sidebar } from "./Sidebar/Sidebar";
 
 const App = () : ReactElement => {
   return (
-    <div className="container-fluid h- border">
-      <div className="row border">
-        <div className="col border">
-          <Sidebar title='Repository'>
-            <div>
-              Local
-            </div>
-            <div>
-              Remote
-            </div>
-            <div>
-              Pull Requests
-            </div>
-            {/* <div>
-              Issues
-            </div>
-            <div>
-              Tags
-            </div>
-            <div>
-              GitHub Actions
-            </div> */}
-          </Sidebar>
-        </div>
-        <div className="col border">
-          Git Tree
-        </div>
-        <div className="col border">
-          <Sidebar title='Commits'>
-            <div>
-              Changes
-            </div>
-            <div>
-              Unstagged Files
-            </div>
-            <div>
-              Staged Files
-            </div>
-            <div>
-              Commit Message
-              <p>
-                Summary
-              </p>
-              <p>
-                Description
-              </p>
-            </div>
-            <div>
-              <button>Commit</button>
-            </div>
-          </Sidebar> 
-        </div>
+    <div className="bg-blue-500 h-screen">
+      <div className="grid grid-cols-4">
+          <div className="bg-red-500">
+            Hello
+          </div>
+          <div className="col-span-2">
+            Content
+          </div>
+          <div className="bg-green-500">
+            World
+          </div>
       </div>
     </div>
+  
+    // <div className="container mx-auto">
+    //   <div className="grid">
+    //     <div className="grid-rows-1">
+    //       <div className="grid-cols-3">
+    //         <Sidebar title='Repository'>
+    //           <div>
+    //             Local
+    //           </div>
+    //           <div>
+    //             Remote
+    //           </div>
+    //           <div>
+    //             Pull Requests
+    //           </div>
+    //           {/* <div>
+    //             Issues
+    //           </div>
+    //           <div>
+    //             Tags
+    //           </div>
+    //           <div>
+    //             GitHub Actions
+    //           </div> */}
+    //         </Sidebar>
+    //       </div>
+    //       <div className="grid-cols-6">
+    //         Git Tree
+    //       </div>
+    //       <div className="grid-cols-3">
+    //         <Sidebar title='Commits'>
+    //           <div>
+    //             Changes
+    //           </div>
+    //           <div>
+    //             Unstagged Files
+    //           </div>
+    //           <div>
+    //             Staged Files
+    //           </div>
+    //           <div>
+    //             Commit Message
+    //             <p>
+    //               Summary
+    //             </p>
+    //             <p>
+    //               Description
+    //             </p>
+    //           </div>
+    //           <div>
+    //             <button>Commit</button>
+    //           </div>
+    //         </Sidebar> 
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/renderer.ts
+++ src/renderer.ts
@@ -28,6 +28,7 @@
 
 // import './index.css';
 import './reset.css';
+import './app.global.css';
 import './Entrypoint';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
